be2d2c3279996206a24b2cb4858a2e64f5013e8d
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2020-11-26 15:54:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const answerInput = document.getElementById('answerInput');
 const submitBtn = document.getElementById('submitBtn');
 const warningDiv = document.getElementById('warningDiv');
 const outputDiv = document.getElementById('outputDiv');
+let keys = Math.max(...Object.keys(localStorage).map(n => parseInt(n)))+1;
+let id = localStorage.length === 0 ? 0 : keys;
+
 
 //Classes
 class FlashCard {
@@ -18,7 +21,8 @@ class FlashCard {
             this.answer = answer;
         }
     }
-
+    
+    //Check input and display message if empty
     static checkInput(question, answer) {
         if(question === '' || answer === '') {
             warningDiv.style.display = 'inline-block';
@@ -32,6 +36,7 @@ class FlashCard {
 }
 
 class UI {
+    //Add card to html
     addFlashCard(flash) {
         let html = `
             <div class="flash">
@@ -39,14 +44,21 @@ class UI {
                 <a href="javascript:;" class="showOrHideBtn" onclick="UI.showOrHide(this)">Show/Hide Answer</a>
                 <h5 class="answer" style="display: none;">${flash.answer}</h5>
                 <div>
-                    <button class="editBtn" onclick="UI.editCard(this)">Edit</button>
-                    <button class="deleteBtn" onclick="UI.deleteCard(this)">Delete</button>
+                    <button id="${id}" class="editBtn" onclick="UI.editCard(this)">Edit</button>
+                    <button id="${id}" class="deleteBtn" onclick="UI.deleteCard(this)">Delete</button>
                 </div>
             </div>
         `;
         outputDiv.innerHTML += html;
     }
 
+    //Store object with question and answer in local storage
+    storeFlash(flash) {
+        localStorage.setItem(`${id}`, JSON.stringify(flash));
+        id++;
+    }
+    
+    //Show or hide answer in a card
     static showOrHide(e) {
         let answer = e.parentNode.children[2];
     
@@ -58,18 +70,48 @@ class UI {
         }
     }
 
+    //Remove card and add values that you want to edit to the input
     static editCard(e) {
+        let itemId = e.id;
         let parentEl = e.parentNode.parentNode;
         questionInput.value = parentEl.children[0].textContent;
         answerInput.value = parentEl.children[2].textContent;
         parentEl.remove();
+        localStorage.removeItem(itemId-1);
     }
 
+    //Remove card from html and object from local storage
     static deleteCard(e) {
+        let itemId = e.id;
         let parentEl = e.parentNode.parentNode;
         parentEl.remove();
+        localStorage.removeItem(itemId-1);
     }
 
+    //Load HTML with local storage data
+    static loadHTML() {
+        if(localStorage.length !== 0) {
+            let entries = Object.entries(localStorage).map(el => {
+                return [JSON.parse(el[0]), JSON.parse(el[1])];
+            });
+            for(let entry of entries) {
+                let html = `
+                <div class="flash">
+                <h4>${entry[1].question}</h4>
+                <a href="javascript:;" class="showOrHideBtn" onclick="UI.showOrHide(this)">Show/Hide Answer</a>
+                <h5 class="answer" style="display: none;">${entry[1].answer}</h5>
+                <div>
+                    <button id="${entry[0]+1}" class="editBtn" onclick="UI.editCard(this)">Edit</button>
+                    <button id="${entry[0]+1}" class="deleteBtn" onclick="UI.deleteCard(this)">Delete</button>
+                </div>
+            </div>
+            `;
+            outputDiv.innerHTML += html;
+            }
+        }
+    }
+
+    //Clear input fields
     clearFields(question, answer) {
         question.value = '';
         answer.value = '';
@@ -77,6 +119,8 @@ class UI {
 }
 
 //Event Listeners
+document.addEventListener('DOMContentLoaded', UI.loadHTML);
+
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let question = questionInput.value;
@@ -88,11 +132,11 @@ submitBtn.addEventListener('click', (e) => {
     //Instantiate UI class
     let ui = new UI();
 
+    //Store Flash Card instance in local storage
+    ui.storeFlash(flashCard);
+
     //Add flash card to output div
     ui.addFlashCard(flashCard);
-
-    //Add flash card to html collection
-    showOrHideBtn = document.getElementsByClassName('showOrHideBtn');
     
     //Empty input fields
     ui.clearFields(questionInput, answerInput);
